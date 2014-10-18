2b2e09eeab1ccadf13947b57bfe4343a9e976efb
Snaevar13 <snaevar13@ru.is>
2014-10-18 12:41:12 +0000

    Updated splitNumbers method to handle new lines and test

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -32,5 +32,8 @@ public class CalculatorTest {
     public void testFourNumbers(){
     	assertEquals(10, Calculator.add("1,2,3,4"));
     }
-
+    @Test
+    public void testNewLine(){
+    	assertEquals(6, Calculator.add("1\n2,3"));
+    }
 }
