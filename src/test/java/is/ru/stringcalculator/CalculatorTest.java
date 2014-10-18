94ab21b7925a864c29982295db56d80e0edb4944
Snaevar13 <snaevar13@ru.is>
2014-10-18 12:16:20 +0000

    Made additional test for 4 numbers

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -28,5 +28,9 @@ public class CalculatorTest {
     public void testMultipleNumbers(){
     	assertEquals(6, Calculator.add("1,2,3"));
     }
+    @Test
+    public void testFourNumbers(){
+    	assertEquals(10, Calculator.add("1,2,3,4"));
+    }
 
 }
